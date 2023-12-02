e54a3503715756c36a3d5631dc9124e20e6c4ea7
snowin-jj <snowin.developer@gmail.com>
2023-12-02 17:51:03 +0530

    update the download page url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        pages: resolve(__dirname, 'download/index.html'),
+        download: resolve(__dirname, 'download.html'),
       },
     },
   },
